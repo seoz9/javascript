023177cec28cee281ef0f63a8c738c8f3ed60a56
seoz9 <namdujamolov@gmail.com>
2024-12-28 20:56:55 +0500

    ping pong tugadi

--- js/pinpong.js
+++ js/pinpong.js
@@ -2,16 +2,34 @@ const score = document.querySelector("#score")
 const gameCount = document.querySelector("#gameCount")
 const playe1 = document.querySelector("#playe1")
 const playe2 = document.querySelector("#playe2")
-const reset = document.querySelector("#playe2")
-let p1,p2
+const reset = document.querySelector("#reset")
+let p1,p2,endGame=2
 setNewGame = ()=>{
     p1=0
     p2=0
+    setScore()
+}
+
+setScore = ()=>{
     score.innerHTML=`${p1} to ${p2}`
 }
 
 setNewGame()
 
+
 gameCount.addEventListener('change',(e)=>{
-    console.log( e.target.value); // Tanlangan qiymatni olish
+    endGame = e.target.value
+})
+
+playe1.addEventListener('click', ()=>{
+    if(p1<endGame) ++p1
+    setScore()
+})
+playe2.addEventListener('click', ()=>{
+    if(p2<endGame) ++p2
+    setScore()
+})
+
+reset.addEventListener('click',()=>{
+    setNewGame()
 })
